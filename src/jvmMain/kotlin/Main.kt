c051e1dc4af7f3d9a024cd23e9b3404e1ba67066
Mohamed Elgohary <bondokelgohary@gmail.com>
2023-07-13 16:11:27 +0300

    cleanup

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,5 +1,6 @@
 import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -7,24 +8,23 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.application
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.window.*
 
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
-
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        Button(onClick = {}) {
+            Text("Mimo")
         }
     }
 }
 
 fun main() = application {
+    val state = rememberWindowState()
+   // state.placement = WindowPlacement.Maximized
     Window(onCloseRequest = ::exitApplication) {
         App()
     }
