b7d94297384c41cf7878a2601a013bda8be06d87
Mohamed Elgohary <bondokelgohary@gmail.com>
2023-07-14 11:18:38 +0300

    add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,17 +25,22 @@ kotlin {
         withJava()
     }
     sourceSets {
-        val ktorVersion = "2.3.2"
         val jvmMain by getting {
             dependencies {
                 implementation(compose.desktop.currentOs)
-                implementation("io.ktor:ktor-client-core:$ktorVersion")
-                implementation("io.ktor:ktor-client-cio:$ktorVersion")
-                implementation("io.ktor:ktor-client-okhttp:$ktorVersion")
-                implementation("io.ktor:ktor-client-java:$ktorVersion")
-                implementation ("io.ktor:ktor-client-serialization:$ktorVersion")
-                implementation ("io.ktor:ktor-client-logging:$ktorVersion")
+                implementation("io.ktor:ktor-client-core:2.3.2")
+                implementation("io.ktor:ktor-client-cio:2.3.2")
+                implementation("io.ktor:ktor-client-okhttp:2.3.2")
+                implementation("io.ktor:ktor-client-java:2.3.2")
+                implementation ("io.ktor:ktor-client-serialization:2.3.2")
+                implementation("io.ktor:ktor-serialization-gson:2.3.2")
+                implementation ("io.ktor:ktor-client-logging:2.3.2")
+                implementation("io.ktor:ktor-client-java:2.3.2")
+                implementation("io.ktor:ktor-client-json:1.6.3")
+                implementation("io.ktor:ktor-client-content-negotiation:2.3.2")
                 implementation ("ch.qos.logback:logback-classic:1.2.3")
+                implementation("io.insert-koin:koin-core:3.4.2")
+                implementation("com.google.code.gson:gson:2.10.1")
             }
         }
         val jvmTest by getting
