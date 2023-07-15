549e1c68d995c954fcacb62d76ede3388c0ab3f5
Mohamed Elgohary <bondokelgohary@gmail.com>
2023-07-15 11:42:28 +0300

    create ui state

---
+++ src/jvmMain/kotlin/presentation/home_screen/HomeUiState.kt
@@ -0,0 +1,36 @@
+package presentation.home_screen
+
+
+/**
+ * Created by Mohamed Elgohary on 7/15/2023.
+ */
+data class HomeUiState(
+    val icon: String = "",
+    val citySearchResult: List<SearchItemUIState> = emptyList(),
+    val forecastDay: List<ForecastDay> = emptyList(),
+    val forecastHour: List<ForecastHour> = emptyList(),
+    val text: String = "",
+    val cloud: Int = 0,
+    val temperatureCelsius: Double = 0.0,
+    val feelsLikeCelsius: Double = 0.0,
+    val localTime: String = "",
+    val name: String = "",
+)
+
+data class SearchItemUIState(
+    val cityName: String = ""
+)
+
+data class ForecastDay(
+    val icon: String = "",
+    val text: String = "",
+    val maximumTemperatureCelsius: Double = 0.0,
+    val minimumTemperatureCelsius: Double = 0.0,
+    val date: String = "",
+)
+
+data class ForecastHour(
+    val icon: String = "",
+    val temperatureCelsius: Double = 0.0,
+    val time: String = "",
+)
