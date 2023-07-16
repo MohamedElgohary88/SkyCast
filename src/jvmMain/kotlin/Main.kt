69fe277198d0ee568444c61b7a1bd00c4aabd4a8
Mohamed Elgohary <bondokelgohary@gmail.com>
2023-07-16 15:53:31 +0300

    call view model

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,24 +1,11 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import app.di.initKoin
 import org.koin.core.Koin
-import org.koin.core.context.GlobalContext.startKoin
-import org.koin.java.KoinJavaComponent.inject
 import presentation.home_screen.HomeScreen
-import presentation.home_screen.view_model.HomeUiState
-import presentation.home_screen.view_model.HomeViewModel
-import presentation.home_screen.view_model.mapper.SearchMapper
 
 @Composable
 @Preview
--- src/jvmMain/kotlin/presentation/home_screen/HomeScreen.kt
+++ src/jvmMain/kotlin/presentation/home_screen/HomeScreen.kt
@@ -1,7 +1,19 @@
 package presentation.home_screen
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import presentation.home_screen.view_model.HomeUiState
 import presentation.home_screen.view_model.HomeViewModel
 
-class HomeScreen(homeViewModel: HomeViewModel) {
+@Composable
+fun HomeScreen(homeViewModel: HomeViewModel) {
+    val state by homeViewModel.homeUiState.collectAsState()
+    HomeContent(state = state)
+    println(homeViewModel.homeUiState)
+}
+
+@Composable
+fun HomeContent(state: HomeUiState) {
 
 }
